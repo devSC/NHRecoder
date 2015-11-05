7e3fb4db62abea995604f811b53f66b1aa6eeb4b
袁仕崇 <xiaochong2154@163.com>
2015-11-05 23:35:16 +0800

    First Commint

---
+++ NHRecoder/NHRecoder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NHRecoder
+//
+//  Created by Wilson-Yuan on 15/11/5.
+//  Copyright © 2015年 Wilson-Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NHRecoder/NHRecoder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NHRecoder
+//
+//  Created by Wilson-Yuan on 15/11/5.
+//  Copyright © 2015年 Wilson-Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
